d4d7c502f10bb13ea9c4cf6f4b962c1bd53f3deb
Sizoborod <se_t610@bk.ru>
2025-01-29 22:14:22 +0300

    Подбил main для старта

--- main.py
+++ main.py
@@ -1,18 +1,79 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from flask import Flask, render_template, redirect, make_response, jsonify, request
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_restful import abort
+from requests import get
+
+from data import db_session, statistic_api, users_api
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from data.users import User
+from data.statistics import Statistic
 
 
-# Press the green button in the gutter to run the script.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
+db_session.global_init("db/base_statistics.db")
+
+
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': f'Not found ({error})'}), 404)
+
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+
+    return "OK"
+
+
+
+'''@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Register', form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register', form=form,
+                                   message="This user already exists")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            email=form.email.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)'''
+
+
+
+
+
+
+def main():
+    db_session.global_init("db/base_statistics.db")
+    # app.register_blueprint(statistic_api.blueprint)
+    app.register_blueprint(users_api.blueprint)
+
+    app.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
